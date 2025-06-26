7d6435b1910bd654e7f2c91156599656567b75be
Praveen yadav <py3008@gmail.com>
2025-06-26 20:09:28 +0530

    update

--- src/WebHook/Notification/EchoesNotification.php
+++ src/WebHook/Notification/EchoesNotification.php
@@ -2,17 +2,27 @@
 
 namespace Netflie\WhatsAppCloudApi\WebHook\Notification;
 
-use Netflie\WhatsAppCloudApi\WebHook\Notification;
-
-abstract class EchoesNotification extends Notification
+class EchoesNotification extends MessageNotification
 {
-    protected ?Support\Customer $customer = null;
     protected string $to;
+    protected string $message_type;
+    protected array $message_data;
 
-    public function __construct(string $id, Support\Business $business, string $to, string $received_at_timestamp)
-    {
-        parent::__construct($id, $business, $received_at_timestamp);
+    public function __construct(
+        string $id,
+        Support\Business $business,
+        string $to,
+        string $message_type,
+        array $message_data,
+        string $timestamp
+    ) {
+        parent::__construct($id, $business, $timestamp);
         $this->to = $to;
+        $this->message_type = $message_type;
+        $this->message_data = $message_data;
+        
+        // Set customer automatically
+        $this->customer = new Support\Customer($to, '', $to);
     }
 
     public function to(): string
@@ -20,14 +30,119 @@ abstract class EchoesNotification extends Notification
         return $this->to;
     }
 
-    public function customer(): ?Support\Customer
+    // Main message method - works for all types
+    public function message(): string
     {
-        return $this->customer;
+        switch ($this->message_type) {
+            case 'text':
+                return $this->message_data['text']['body'] ?? '';
+            case 'button':
+                return $this->message_data['button']['text'] ?? '';
+            case 'interactive':
+                return $this->message_data['interactive']['nfm_reply']['body'] ?? '';
+            default:
+                return $this->message_data[$this->message_type]['caption'] ?? '';
+        }
     }
 
-    public function withCustomer(Support\Customer $customer): self
+    // Text methods
+    public function text(): string
     {
-        $this->customer = $customer;
-        return $this;
+        return $this->message_data['text']['body'] ?? '';
+    }
+
+    // Media methods
+    public function mediaId(): string
+    {
+        return $this->message_data[$this->message_type]['id'] ?? '';
+    }
+
+    public function mimeType(): string
+    {
+        return $this->message_data[$this->message_type]['mime_type'] ?? '';
+    }
+
+    public function sha256(): string
+    {
+        return $this->message_data[$this->message_type]['sha256'] ?? '';
+    }
+
+    public function filename(): string
+    {
+        return $this->message_data[$this->message_type]['filename'] ?? '';
+    }
+
+    public function caption(): string
+    {
+        return $this->message_data[$this->message_type]['caption'] ?? '';
+    }
+
+    // Button methods
+    public function buttonText(): string
+    {
+        return $this->message_data['button']['text'] ?? '';
+    }
+
+    public function payload(): string
+    {
+        return $this->message_data['button']['payload'] ?? '';
+    }
+
+    // Flow methods
+    public function name(): string
+    {
+        return $this->message_data['interactive']['nfm_reply']['name'] ?? '';
+    }
+
+    public function body(): string
+    {
+        return $this->message_data['interactive']['nfm_reply']['body'] ?? '';
+    }
+
+    public function responseJson(): string
+    {
+        return $this->message_data['interactive']['nfm_reply']['response_json'] ?? '';
+    }
+
+    // Override parent methods since echo messages don't have context
+    public function replyingToMessageId(): ?string
+    {
+        return null;
+    }
+
+    public function isForwarded(): bool
+    {
+        return false;
+    }
+
+    public function context(): ?Support\Context
+    {
+        return null;
+    }
+
+    // Type checking methods for instanceof checks
+    public function isText(): bool
+    {
+        return $this->message_type === 'text';
+    }
+
+    public function isMedia(): bool
+    {
+        return in_array($this->message_type, ['image', 'video', 'audio', 'document', 'voice', 'sticker']);
+    }
+
+    public function isButton(): bool
+    {
+        return $this->message_type === 'button';
+    }
+
+    public function isFlow(): bool
+    {
+        return $this->message_type === 'interactive' && isset($this->message_data['interactive']['nfm_reply']);
+    }
+
+    public function isInteractive(): bool
+    {
+        return $this->message_type === 'interactive';
     }
 }
--- src/WebHook/Notification/EchoesNotificationFactory.php
+++ src/WebHook/Notification/EchoesNotificationFactory.php
@@ -4,7 +4,7 @@ namespace Netflie\WhatsAppCloudApi\WebHook\Notification;
 
 class EchoesNotificationFactory
 {
-    public function buildFromPayload(array $value, string $id, string $field)
+    public function buildFromPayload(array $value, string $id, string $field): ?EchoesNotification
     {
         if (!isset($value['message_echoes'][0])) {
             return null;
@@ -13,136 +13,13 @@ class EchoesNotificationFactory
         $message = $value['message_echoes'][0];
         $metadata = $value['metadata'] ?? [];
         
-        $notification = $this->buildEchoesNotification($metadata, $message);
-
-        return $this->decorateNotification($notification, $message, $value);
-    }
-
-    private function buildEchoesNotification(array $metadata, array $message)
-    {
-        switch ($message['type']) {
-            case 'text':
-                return new Text(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['text']['body'],
-                    $message['timestamp']
-                );
-            case 'reaction':
-                return new Reaction(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['reaction']['message_id'],
-                    $message['reaction']['emoji'] ?? '',
-                    $message['timestamp']
-                );
-            case 'sticker':
-            case 'image':
-            case 'document':
-            case 'audio':
-            case 'video':
-            case 'voice':
-                return new Media(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message[$message['type']]['id'],
-                    $message[$message['type']]['mime_type'],
-                    $message[$message['type']]['sha256'],
-                    $message[$message['type']]['filename'] ?? '',
-                    $message[$message['type']]['caption'] ?? '',
-                    $message['timestamp']
-                );
-            case 'location':
-                return new Location(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['location']['latitude'],
-                    $message['location']['longitude'],
-                    $message['location']['name'] ?? '',
-                    $message['location']['address'] ?? '',
-                    $message['timestamp']
-                );
-            case 'contacts':
-                return new Contact(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['contacts'][0]['addresses'] ?? [],
-                    $message['contacts'][0]['emails'] ?? [],
-                    $message['contacts'][0]['name'],
-                    $message['contacts'][0]['org'] ?? [],
-                    $message['contacts'][0]['phones'],
-                    $message['contacts'][0]['urls'] ?? [],
-                    $message['contacts'][0]['birthday'] ?? null,
-                    $message['timestamp']
-                );
-            case 'button':
-                return new Button(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['button']['text'],
-                    $message['button']['payload'],
-                    $message['timestamp']
-                );
-            case 'interactive':
-                if (isset($message['interactive']['nfm_reply'])) {
-                    $nfmReply = $message['interactive']['nfm_reply'];
-
-                    return new Flow(
-                        $message['id'],
-                        new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                        $nfmReply['name'],
-                        $nfmReply['body'],
-                        $nfmReply['response_json'] ?? '',
-                        $message['timestamp'],
-                    );
-                }
-
-                return new Interactive(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['interactive']['list_reply']['id'] ?? $message['interactive']['button_reply']['id'],
-                    $message['interactive']['list_reply']['title'] ?? $message['interactive']['button_reply']['title'],
-                    $message['interactive']['list_reply']['description'] ?? '',
-                    $message['timestamp']
-                );
-            case 'order':
-                return new Order(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['order']['catalog_id'],
-                    $message['order']['text'] ?? '',
-                    new Support\Products($message['order']['product_items']),
-                    $message['timestamp']
-                );
-            case 'system':
-                return new System(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    new Support\Business($message['system']['customer'], ''),
-                    $message['system']['body'],
-                    $message['timestamp']
-                );
-            case 'unknown':
-            default:
-                return new Unknown(
-                    $message['id'],
-                    new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
-                    $message['timestamp']
-                );
-        }
-    }
-
-    private function decorateNotification($notification, array $message, array $value)
-    {
-        // For echoes, we can create a customer object from the 'to' field
-        if (isset($message['to'])) {
-            $notification->withCustomer(new Support\Customer(
-                $message['to'],
-                '', // Name is not available in echoes
-                $message['to']
-            ));
-        }
-
-        return $notification;
+        return new EchoesNotification(
+            $message['id'],
+            new Support\Business($metadata['phone_number_id'], $metadata['display_phone_number']),
+            $message['to'],
+            $message['type'],
+            $message,
+            $message['timestamp']
+        );
     }
 }
